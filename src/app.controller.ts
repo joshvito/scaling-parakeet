c85c33d2f41d96338c314acb4a98f5d150825801
joshvito <josh.vito@gmail.com>
2025-06-27 22:37:10 -0400

    fix trest

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,25 +1,27 @@
-import { All, Controller, Req, Res } from '@nestjs/common';
+import { All, Controller, Req } from '@nestjs/common';
 import type { Request } from 'express';
 import fetch, { RequestInit } from 'node-fetch';
 
 @Controller()
 export class AppController {
-  constructor() {
-  }
+  constructor() {}
 
   @All('*')
   async proxy(@Req() req: Request) {
-    const targetBaseUrl = process.env.DOWNSTREAM_SERVER_LOCATION ?? 'http://swapi.py4e.com/api';
+    const targetBaseUrl =
+      process.env.DOWNSTREAM_SERVER_LOCATION ?? 'http://swapi.py4e.com/api';
     const targetUrl = `${targetBaseUrl}${req.originalUrl}`;
     const response = await fetch(targetUrl, {
       method: req.method,
       headers: {},
       body: req.body,
-      params: new URLSearchParams(req.query as Record<string,string>),
+      params: new URLSearchParams(req.query as Record<string, string>),
     } as RequestInit);
 
-    return response.ok 
+    return response.ok
       ? await response.json()
-      : new Error(response.statusText, {cause: {status: response.status, message: response.statusText}})
+      : new Error(response.statusText, {
+          cause: { status: response.status, message: response.statusText },
+        });
   }
 }
